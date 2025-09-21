5dd33c717fd89a13d689485dc9b54d2d70a9ec1f
authbytegirl <khushic142@gmail.com>
2025-09-21 13:29:57 +0530

    Simplify blog app

---
+++ miniblog/src/main/java/com/example/miniblog/MiniBlogApplication.java
@@ -0,0 +1,4 @@
+package com.example.miniblog;
+
+public class MiniBlogApplication {
+}
--- miniblog/src/main/java/com/example/miniblog/model/BlogPost.java
+++ miniblog/src/main/java/com/example/miniblog/model/BlogPost.java
@@ -1,34 +1,4 @@
 package com.example.miniblog.model;
 
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "posts")
 public class BlogPost {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String title;
-
-    @Column(length = 5000)
-    private String content;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")  // foreign key column in posts table
-    private User user;
-
-    // Getters & setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
-
-    public User getUser() { return user; }
-    public void setUser(User user) { this.user = user; }
 }
